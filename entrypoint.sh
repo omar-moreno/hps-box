572f173f8b5bceef83faeb68efa452c02e42d2c8
Omar Moreno <email@omarmoreno.net>
2023-03-21 23:09:56 -0700

    Remove deprecated files.

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-#source /software/geant4/install/bin/geant4.sh
-
-cd $1
-
-eval "${@:2}"
--- src/hps/prod/wabs.py
+++
@@ -1,8 +0,0 @@
-
-import hps
-
-def produce_wabs(energy : float, run : int): 
-    command = '/software/hps-mg/mg4/wabs/wab_template/bin/ ' \
-            './generate_events; cp ../Events/* /current'
-    
-    hps.execute(command)
